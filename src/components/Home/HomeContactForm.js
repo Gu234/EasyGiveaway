f07dd28bbf791f9497e826a9ed23d7569fee7314
Gustaw Filipek <guciofilipek@gmail.com>
2019-12-19 17:38:56 +0100

    validation

--- src/components/Home/HomeContactForm.js
+++ src/components/Home/HomeContactForm.js
@@ -8,13 +8,57 @@ export default class HomeContactForm extends Component {
       name: '',
       email: '',
       textarea: '',
-      errorMsg: []
+      error:false,
+      succes:false
    }
 
    handleChange = (e) => {
       this.setState({
          [e.target.name]: e.target.value
       })
+   }
+
+   handleSubmit = (e) => {
+      e.preventDefault();
+      if(this.validate())
+         this.sendMsg() 
+      else 
+      this.setState({
+         error:true
+      })
+   }
+
+   validate = () => {
+      return this.validateName() && this.validateEmail() && this.validateTextarea() 
+   }
+
+   validateName = (name) => {
+      if (!name) return false
+      if (/\s/.test(name)) return false
+      return true
+   }
+
+   validateEmail = (email) => {
+      const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+
+   validateTextarea = (text) => {
+      if (!text) return false
+      if (text.length<120) return false
+      return true
+   }
+   sendMsg = (payload) => {
+      
+      const url = 'https://fer-api.coderslab.pl/v1/portfolio/contact';
+      const data = {
+         'method': 'POST',
+         'Content-Type': "application/json",
+         'payload':payload
+      }
+      
+      fetch(url,data).then()
+
    }
 
    render() {
@@ -39,7 +83,7 @@ export default class HomeContactForm extends Component {
                         <div>Wpisz swoją wiadomość</div>
                         <textarea placeholder='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et accusantium sunt nihil reiciendis at fugiat neque harum pariatur similique fugit voluptatem, unde sit incidunt. Reiciendis nesciunt perferendis id aliquam alias.' onChange={this.handleChange} name='textarea' value={this.state.textarea} type='text' ></textarea>
                      </div>
-                     <div className="HomeContactForm-inputs-send" type="submit">Wyślij</div>
+                     <div className="HomeContactForm-inputs-send" onClick={this.handleSubmit} type="submit">Wyślij</div>
                   </form>
                </div>
             </div>
