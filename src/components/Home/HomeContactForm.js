d64375cd54a05d0f245beb200eae842160ef6412
Gustaw Filipek <guciofilipek@gmail.com>
2019-12-20 17:06:19 +0100

    contactForm style and message sending

--- src/components/Home/HomeContactForm.js
+++ src/components/Home/HomeContactForm.js
@@ -8,8 +8,10 @@ export default class HomeContactForm extends Component {
       name: '',
       email: '',
       textarea: '',
-      error:false,
-      succes:false
+      isNameOk: true,
+      isEmailOk: true,
+      isTextareaOk: true,
+      succes: false
    }
 
    handleChange = (e) => {
@@ -19,17 +21,23 @@ export default class HomeContactForm extends Component {
    }
 
    handleSubmit = (e) => {
+      this.setState({succes:false})
       e.preventDefault();
-      if(this.validate())
-         this.sendMsg() 
-      else 
-      this.setState({
-         error:true
-      })
+      if (this.validate())
+         this.sendMsg(this.state)
+     
    }
 
    validate = () => {
-      return this.validateName() && this.validateEmail() && this.validateTextarea() 
+      const isNameOk = this.validateName(this.state.name);
+      const isEmailOk = this.validateEmail(this.state.email);
+      const isTextareaOk = this.validateTextarea(this.state.textarea);
+
+      this.setState({isNameOk:isNameOk});
+      this.setState({isEmailOk:isEmailOk});
+      this.setState({isTextareaOk:isTextareaOk});
+
+      return isNameOk && isEmailOk && isTextareaOk
    }
 
    validateName = (name) => {
@@ -41,23 +49,31 @@ export default class HomeContactForm extends Component {
    validateEmail = (email) => {
       const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(email);
-    }
+   }
 
    validateTextarea = (text) => {
       if (!text) return false
-      if (text.length<120) return false
+      if (text.length < 120) return false
       return true
    }
    sendMsg = (payload) => {
-      
+
       const url = 'https://fer-api.coderslab.pl/v1/portfolio/contact';
       const data = {
          'method': 'POST',
-         'Content-Type': "application/json",
-         'payload':payload
+         'headers': {
+            'Content-Type': 'application/json'
+         },
+         'body':JSON.stringify({
+            'name': payload.name,
+            'email': payload.email,
+            'message': payload.textarea
+         })
       }
-      
-      fetch(url,data).then()
+
+      fetch(url, data).then(r => {
+         if (r.ok) this.setState({succes:true})
+      })
 
    }
 
@@ -68,20 +84,24 @@ export default class HomeContactForm extends Component {
                <div className='HomeContactForm-baner'></div>
                <div className='HomeContactForm-secondHalf'>
                   <SectionTitle>Skontaktuj się z nami</SectionTitle>
+                  <div className={this.state.succes ? 'succes':''}>Wiadomość została wysłana!<br/> Wkrótce się skontaktujemy.</div>
                   <form className='HomeContactForm-inputs' onSubmit={this.handleSubmit}>
                      <div className='HomeContactForm-inputs-upper'>
                         <div className='HomeContactForm-inputs-name'>
                            <div>Wpisz swoje imię</div>
                            <input placeholder='Krzysztof' onChange={this.handleChange} name='name' value={this.state.name} type='text' className="form-control"></input>
+                           <div className={this.state.isNameOk ? '':'invalidInput'}>Podane imię jest nieprawidłowe!</div>
                         </div>
                         <div className='HomeContactForm-inputs-email'>
                            <div>Wpisz swój email</div>
                            <input placeholder='abc@xyz.pl' onChange={this.handleChange} name='email' value={this.state.email} className="form-control" type='email'></input>
+                           <div className={this.state.isEmailOk ? '':'invalidInput'}>Podany email jest nieprawidłowy!</div>
                         </div>
                      </div>
                      <div className='HomeContactForm-inputs-text'>
                         <div>Wpisz swoją wiadomość</div>
                         <textarea placeholder='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et accusantium sunt nihil reiciendis at fugiat neque harum pariatur similique fugit voluptatem, unde sit incidunt. Reiciendis nesciunt perferendis id aliquam alias.' onChange={this.handleChange} name='textarea' value={this.state.textarea} type='text' ></textarea>
+                        <div className={this.state.isTextareaOk ? '':'invalidInput'}>Wiadomość musi mieć conajmniej 120 znaków!</div>
                      </div>
                      <div className="HomeContactForm-inputs-send" onClick={this.handleSubmit} type="submit">Wyślij</div>
                   </form>
