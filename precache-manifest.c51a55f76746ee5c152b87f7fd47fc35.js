d81843207c0e8eea15faaa336bb91c5da07672e1
Gustaw Filipek <guciofilipek@gmail.com>
2020-01-04 12:20:16 +0100

    Updates

---
+++ precache-manifest.c51a55f76746ee5c152b87f7fd47fc35.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0e629925296053d35bfef11c1c097a",
+    "url": "/easy-giveaway/index.html"
+  },
+  {
+    "revision": "03a537455378a66cbbcb",
+    "url": "/easy-giveaway/static/css/main.7a068807.chunk.css"
+  },
+  {
+    "revision": "b2712dedcc6fe4cb32c7",
+    "url": "/easy-giveaway/static/js/2.d247f8e1.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/easy-giveaway/static/js/2.d247f8e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03a537455378a66cbbcb",
+    "url": "/easy-giveaway/static/js/main.7ef3989f.chunk.js"
+  },
+  {
+    "revision": "f08671360ae08cc3b48c",
+    "url": "/easy-giveaway/static/js/runtime-main.7537bae8.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/easy-giveaway/static/media/3_Columns_Background.99f2e620.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/easy-giveaway/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/easy-giveaway/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/easy-giveaway/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/easy-giveaway/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/easy-giveaway/static/media/Form-Hero-Image.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/easy-giveaway/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/easy-giveaway/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/easy-giveaway/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/easy-giveaway/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/easy-giveaway/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/easy-giveaway/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/easy-giveaway/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/easy-giveaway/static/media/Signature.e54cfcfc.svg"
+  }
+]);
